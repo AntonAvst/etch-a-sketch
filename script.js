09bafb0cdff5bb7f144729d53f8ec612c7d83176
anton <antonavst@gmail.com>
2024-09-11 11:23:03 +0300

    done

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 function createTwoDivs() {
-  // Create first div
-  const firstDiv = document.createElement("div");
-  firstDiv.textContent = "This is the first div!";
-  firstDiv.classList.add("flex-item");
-
-  // Create second div
-  const secondDiv = document.createElement("div");
-  secondDiv.textContent = "This is the second div!";
-  secondDiv.classList.add("flex-item");
-
-  // Append the new divs to the container div
+  var rows = 16;
+  var cols = 16;
+  var container_width = 640;
+  var container_height = 640;
+  var div_width = container_width/cols;
+  var div_height = container_height/rows;
   const container = document.getElementById("container");
-  container.appendChild(firstDiv);
-  container.appendChild(secondDiv);
+  container.innerHTML = ''; // Clear any existing content
+
+  for (var i=0; i<rows; i++){
+    for (var j=0; j<cols; j++){
+        const new_div = document.createElement("div");
+        new_div.classList.add("flex-item");
+
+        //Set position
+        new_div.style.left = j*div_width + "px";
+        new_div.style.top = i*div_height + "px";
+
+        new_div.addEventListener('mouseover', function(){
+          new_div.classList.add('hovered');
+        })
+
+        container.appendChild(new_div);
+    }
+  }
 }
